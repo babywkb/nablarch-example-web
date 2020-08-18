f42e1dfc8b02147fdaddc59dd2a2ee61e04b25b0
opengl-8080 <opengl-8080@users.noreply.github.com>
2020-08-18 15:24:59 +0900

    Micrometerアダプタ検証準備

---
+++ src/main/java/com/nablarch/example/app/metrics/CustomMeterBinderListProvider.java
@@ -0,0 +1,17 @@
+package com.nablarch.example.app.metrics;
+
+import io.micrometer.core.instrument.binder.MeterBinder;
+import nablarch.integration.micrometer.DefaultMeterBinderListProvider;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CustomMeterBinderListProvider extends DefaultMeterBinderListProvider {
+
+    @Override
+    protected List<MeterBinder> createMeterBinderList() {
+        return Arrays.asList(
+//            new TomcatMetrics(null, null)
+        );
+    }
+}
